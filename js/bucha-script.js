c8d290b96affc11b3ed9e6371a01c3cc8a4e2a5f
nycsamuel <xsangmin@gmail.com>
2018-01-06 15:44:33 -0500

    Disable carousel if width greater than 599

--- js/bucha-script.js
+++ js/bucha-script.js
@@ -20,48 +20,52 @@
     const length = imgArray.length - 1;
     var index;
 
-    imgBox.on('click', function (event) {
-      // prevent scrolling when modal is opened
-      $('body').css('overflow', 'hidden');
-      // show carousel overlay
-      carousel.show();
-      // add image src using the image array
-      // event.target.dataset.box gets the data attribute from img-box elements
-      index = event.target.dataset.box;
-      $('.carousel-img').attr('src', imgArray[index]);
-    });
+    let screenWidth = $('body').width();
 
-    closeBtn.on('click', function (event) {
-      // return scrolling after closing modal
-      $('body').css('overflow', 'auto');
-      // hide carousel modal
-      carousel.hide();
-    });
+    if (screenWidth > 599) {
+      imgBox.on('click', function (event) {
+        // prevent scrolling when modal is opened
+        $('body').css('overflow', 'hidden');
+        // show carousel overlay
+        carousel.show();
+        // add image src using the image array
+        // event.target.dataset.box gets the data attribute from img-box elements
+        index = event.target.dataset.box;
+        $('.carousel-img').attr('src', imgArray[index]);
+      });
 
-    left.on('click', function (event) {
-      index--; // decrement 
-      if (index < 0) { // keep carousel looping
-        index = length;
-      }
-      // set the img src to the previous image
-      $('.carousel-img').attr('src', imgArray[index]);
-    });
-
-    right.on('click', function (event) {
-      index++; // increment
-      if (index > length) { // keep carousel looping
-        index = 0;
-      }
-      // set the img src to the next image
-      $('.carousel-img').attr('src', imgArray[index]);
-    });
-
-    window.onkeyup = function (event) {
-      let key = event.keyCode ? event.keyCode : event.which;
-      if (key == 27) {
+      closeBtn.on('click', function (event) {
+        // return scrolling after closing modal
         $('body').css('overflow', 'auto');
+        // hide carousel modal
         carousel.hide();
-      }
-    };
+      });
+
+      left.on('click', function (event) {
+        index--; // decrement 
+        if (index < 0) { // keep carousel looping
+          index = length;
+        }
+        // set the img src to the previous image
+        $('.carousel-img').attr('src', imgArray[index]);
+      });
+
+      right.on('click', function (event) {
+        index++; // increment
+        if (index > length) { // keep carousel looping
+          index = 0;
+        }
+        // set the img src to the next image
+        $('.carousel-img').attr('src', imgArray[index]);
+      });
+
+      window.onkeyup = function (event) {
+        let key = event.keyCode ? event.keyCode : event.which;
+        if (key == 27) {
+          $('body').css('overflow', 'auto');
+          carousel.hide();
+        }
+      };
+    }
   });
 })();
